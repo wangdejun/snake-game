31f3cf4c09ccad8f9a74d3a98790a7195f47117a
wangdejun <1074201799@qq.com>
2018-03-02 23:57:54 +0800

    fix

--- ReduxTest/DestructingSpreadingRest.js
+++ ReduxTest/DestructingSpreadingRest.js
@@ -1,9 +1,31 @@
 const person = {
-    a: 'Bob',
-    b:'Smith',
+    firstName: 'Bob',
+    secondName: 'Smith',
+    name: 'wangdejun',
+    age: '29'
 }
 
-const { a, b} = person;
-
+let { a, b} = person;
+const {firstName, secondName,...otherProperties} = person;
 console.log(a);
 console.log(b);
+console.log("-------")
+console.log(firstName);
+console.log(secondName);
+console.log(otherProperties);
+
+const {firstName : c, secondName : d} = person;
+// rename the variable;
+console.log('------------');
+console.log('After rename the name');
+console.log(c);
+console.log(d);
+
+
+const colors = ['red', 'blue', 'green', 'purple'];
+
+const [favColor, secondFavColor, ...otherColors] = colors;
+
+console.log(favColor);
+console.log(secondFavColor);
+console.log(otherColors);
